16898f66590b8ccbc049d68cc16a2cf96a185bcd
namandangi <danginaman55@gmail.com>
2020-01-11 18:52:26 +0530

    add search feature

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ var express = require('express'),
     var chapterNumber = 1;
     var mangaNameList = [];
     var chapterLink = [{id : Number , name : String , title : String , url : String}];
-    var mangaSearchList = [];
-    var mangaSearch = [{title : String , name : String , url : String}];
+    // var mangaSearchList = [];
+    // var mangaSearch = [{title : String , name : String , url : String}];
     var mangaList = [{title : String , name : String , url : String}];
-    let urlD = "https://www.mangazuki.online/manga/springtime-for-blossom-comic/chapter-28/";
-    let surl = "https://www.mangazuki.online/?s=spring&post_type=wp-manga";
-    let url = "https://www.mangazuki.online/manga/winner-takes-all/";
+//     let urlD = "https://www.mangazuki.online/manga/springtime-for-blossom-comic/chapter-28/";
+//    // let surl = "https://www.mangazuki.online/?s=spring&post_type=wp-manga";
+//     let url = "https://www.mangazuki.online/manga/winner-takes-all/";
     var imgs = [];
 
     app.get('/mangas',(req,res)=>{
@@ -80,17 +80,17 @@ var express = require('express'),
 });
 
     app.get('/search',(req,res)=>{
-        console.log(req.body.Search);
-        //let surl = "https://www.mangazuki.online/?s="+req.body.name+"&post_type=wp-manga"        
+        console.log(req.query.search);
+        let surl = "https://www.mangazuki.online/?s="+req.query.search.toString().split(' ').join('&')+"&post_type=wp-manga"        
         axios.get(surl).then(response=>{
             let $ = cheerio.load(response.data);
             $('.h4').children().each(function(i,manga){
-                mangaSearchList[i] = $(manga).text().toString().replace(/\s+/g, '-').toLowerCase();
-                mangaSearch[i]   = {title : $(manga).text() , name : mangaSearchList[i], url : $(manga).attr('href')};
+                mangaNameList[i] = $(manga).text().toString().replace(/\s+/g, '-').toLowerCase();
+              mangaList[i]    = {title : $(manga).text() , name : mangaNameList[i], url : $(manga).attr('href')};
                 //console.log(mangaSearch[i]);
         });
     });
-    res.render("search.ejs",{mangas:mangaSearch});
+    res.render("search.ejs",{list:mangaList});
 });
 
     app.get('/anime',(req,res)=>{
@@ -105,27 +105,27 @@ var express = require('express'),
    
 
  
-app.get("/manga-search/:name/:id",(req,res)=>{
-    let id;
-    //console.log(req.params.name);
-    chapterNumber = req.params.id;
-    for(var i=0;i<mangaSearch.length;i++)
-        {
-            if(req.params.name == mangaSearch[i].name)
-            id = i;
-        }
+// app.get("/manga-search/:name/:id",(req,res)=>{
+//     let id;
+//     //console.log(req.params.name);
+//     chapterNumber = req.params.id;
+//     for(var i=0;i<mangaSearch.length;i++)
+//         {
+//             if(req.params.name == mangaSearch[i].name)
+//             id = i;
+//         }
 
-    //var url = mangaList[id].url+"/chapter-"+chapterNumber;
-    var url = mangaSearch[id].url+"/chapter-"+chapterNumber;
-    axios.get(url).then(response=>{
-    let $ = cheerio.load(response.data);
-    //    let img = $('#image-0').attr('src');
-    $('.wp-manga-chapter-img').each(function(i,img){
-        imgs[i] = $(this).attr('src');
-    })
-    res.render("index.ejs",{images:imgs,name:mangaSearch[id].name,number:chapterNumber});
-});
-});
+//     //var url = mangaList[id].url+"/chapter-"+chapterNumber;
+//     var url = mangaSearch[id].url+"/chapter-"+chapterNumber;
+//     axios.get(url).then(response=>{
+//     let $ = cheerio.load(response.data);
+//     //    let img = $('#image-0').attr('src');
+//     $('.wp-manga-chapter-img').each(function(i,img){
+//         imgs[i] = $(this).attr('src');
+//     })
+//     res.render("index.ejs",{images:imgs,name:mangaSearch[id].name,number:chapterNumber});
+// });
+// });
 
     app.listen(process.env.PORT||3000,process.env.IP,()=>{
         console.log(`server running on port 3000`);
