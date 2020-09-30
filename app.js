e13cd54e3bbeb62c3466f71f397272b49d8e7af9
Naman <45919359+namandangi@users.noreply.github.com>
2020-10-01 00:32:21 +0530

    Devsetup (#6)
    * setup eslint dependencies
    
    * setup prettier dependencies
    
    * update ignore files and add vscode dep.
    
    * prettify code

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 const express = require('express');
-const rp      = require("request-promise");
-const axios   = require('axios');
+const rp = require('request-promise');
+const axios = require('axios');
 const cheerio = require('cheerio');
 const mongoose = require('mongoose');
-    app = express();   
+const dotEnv = require('dotenv');
+const morgan = require('morgan');
 
-    mongoose.connect("mongodb://localhost:27017/comic_test",{ useNewUrlParser : true });
-    app.set("view engine","ejs");
-    app.use(express.static(__dirname + "/public"));
+app = express();
 
-    const mangaRouter  = require('./routes/mangas');
+mongoose.connect('mongodb://localhost:27017/comic_test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/public`));
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms'),
+);
+dotEnv.config();
 
-    app.use('',mangaRouter);
+const mangaRouter = require('./routes/mangas');
 
-    app.listen(process.env.PORT||3000,process.env.IP,()=>{
-        console.log(`server running on port 3000`);
-    })
+app.use('', mangaRouter);
+
+app.listen(process.env.PORT || 3000, process.env.IP, () => {
+  console.log(`server running on port ${process.env.PORT}`);
+});
