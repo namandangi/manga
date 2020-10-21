5c1e123b100c84dddc6dba519ec12161b82b112b
namandangi <danginaman55@gmail.com>
2020-10-21 22:25:38 +0530

    update to ts and sass

---
+++ webapp/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from './components/home';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+       <div className="App">      
+          <Home />
+    </div>
+    </div>
+  );
+}
+
+export default App;
