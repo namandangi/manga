9fb8dfc941fb99102a4d5407597d66b829a10132
namandangi <danginaman55@gmail.com>
2020-10-24 03:10:05 +0530

    include partials and manga list

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Home from './components/home';
+import MangaList from './components/mangaList';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-       <div className="App">      
-          <Home />
-    </div>
+      <div className="App">
+        {/* <Home /> */}
+        <MangaList />
+      </div>
     </div>
   );
 }
