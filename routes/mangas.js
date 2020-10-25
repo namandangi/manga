dee9cf6e087e9c2ce2aab89a40c59f9e4ff7a1be
namandangi <danginaman55@gmail.com>
2020-10-25 23:54:49 +0530

    add route for manga details

--- routes/mangas.js
+++ routes/mangas.js
@@ -5,9 +5,10 @@ const controller = require('../controllers/mangas');
 const router = Router();
 
 router.get('/mangas', asyncHandler(controller.getMangas));
+router.get('/mangas/details/:id', asyncHandler(controller.getMangaDetail));
 router.get('/mangas/read/:name', asyncHandler(controller.getMangaByName));
 router.get('/mangas/read/:name/:id', asyncHandler(controller.getMangaChapter));
 router.get('/mangas/search', asyncHandler(controller.searchManga));
-router.get('/mangas/:tag', asyncHandler(controller.getByTag));
+router.get('/mangas/tag/:name', asyncHandler(controller.getByTag));
 
 module.exports = router;
