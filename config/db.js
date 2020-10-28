19c4d957eb296d3d66ca2cbbabc9d247fdfa1199
namandangi <danginaman55@gmail.com>
2020-10-28 16:12:18 +0530

    debug env vars

--- config/db.js
+++ config/db.js
@@ -1,13 +1,15 @@
 /* eslint-disable operator-linebreak */
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
-const { dbConnectionUrl, dbTestConnectionUrl } = require('./constant');
+// const { dbConnectionUrl, dbTestConnectionUrl } = require('./constant');
 
 module.exports = {
   dbConnection: async () => {
     try {
       const dbConnUrl =
-        process.env.NODE_ENV === 'test' ? dbTestConnectionUrl : dbConnectionUrl;
+        process.env.NODE_ENV === 'test'
+          ? 'mongodb+srv://naman:naman007@cluster0.xnged.mongodb.net/manga_test?retryWrites=true&w=majority'
+          : 'mongodb+srv://naman:naman007@cluster0.xnged.mongodb.net/manga?retryWrites=true&w=majority';
       await mongoose.connect(dbConnUrl, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
