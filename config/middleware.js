629f32a022f8ca26e4dd06ab616e08958cf3303e
namandangi <danginaman55@gmail.com>
2020-10-28 15:30:20 +0530

    add missing headers for cors

--- config/middleware.js
+++ config/middleware.js
@@ -2,7 +2,11 @@ module.exports = {
   options: {
     origin: '*',
     methods: ['GET', 'POST', 'HEAD', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Access-Control-Allow-Origin',
+    ],
     preflightContinue: false,
     optionsSuccessStatus: 204 || 200,
   },
